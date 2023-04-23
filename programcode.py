2a47bcae455a2005b86da662d4d17e805b3d3767
Gene014 <calmaeugene06@gmail.com>
2023-04-23 14:53:40 +0800

    print gwa of users

---
+++ programcode.py
@@ -0,0 +1,16 @@
+# Calma, Eugene Marie S.
+# BSCPE_1-4
+# OOP Laboratory Exercise Number 3 Problem 2 (GWA-MAKER)
+
+#Write a Python program that read a file containing the name of 20 students together with their GWA. 
+#The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+def process():
+    #open gwa.txt (read)
+    with open("gwa.txt","r") as input_file:
+        #read gwa.txt line by line
+        for line in input_file:
+            input_gwa = str(line)
+            gwa=(input_gwa)
+            print(gwa)
+process ()
