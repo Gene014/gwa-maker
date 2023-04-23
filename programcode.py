4aa2e89a9b0b53fc865df88d6f53e98f5db6acd3
Gene014 <calmaeugene06@gmail.com>
2023-04-23 22:13:24 +0800

    running but wrong gwa

--- programcode.py
+++ programcode.py
@@ -5,17 +5,21 @@
 #Write a Python program that read a file containing the name of 20 students together with their GWA. 
 #The program will outputs the name of the student who got the highest GWA (including the GWA).
 
-def process():
-    #set variables for max gwa and for the uname
-    max_gwa=1
-    max_name=""
+
+#set variables for max gwa and for the uname
+max_gwa=1
+max_name=""
     #open gwa.txt (read)
-    with open("gwa.txt","r") as input_file:
+with open("gwa.txt","r") as input_file:
         #read gwa.txt line by line
         for line in input_file:
-            variables=line.strip().rsplit('',1)
-            if len(variables) ==2:
-                name, gwa = variables
-                
-            
-process ()
+            profile=line.strip().rsplit(' ',1)
+            if len(profile) ==2:
+                name, gwa = profile
+            #convert gwa to float
+                gwa=float(gwa)
+                if gwa <= max_gwa: 
+                    #update the maximum gwa and student name
+                    max_gwa= gwa
+                    max_name= name
+        print(f"The student with the highest GWA is {max_name} with a GWA of {max_gwa:.2f}.")
