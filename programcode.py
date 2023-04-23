cf6f73b4e33d006691ed401b6d9df0429507a6f5
Gene014 <calmaeugene06@gmail.com>
2023-04-23 22:54:28 +0800

    finishing touches, program running

--- programcode.py
+++ programcode.py
@@ -5,7 +5,9 @@
 #Write a Python program that read a file containing the name of 20 students together with their GWA. 
 #The program will outputs the name of the student who got the highest GWA (including the GWA).
 
-
+#greetings for the user
+account_name = input("\n\33[33mHi, Good day! Please enter your name... Checking the text file...\33[0m ")
+print(f"\n\33[1m Hi {account_name}! Please wait...Uploading your GWA")
 #set variables for max gwa and for the uname
 max_gwa=0
 max_name=""
@@ -27,4 +29,6 @@ with open("gwa.txt","r") as input_file:
                     max_gwa= gwa
                     max_name= name
                     #print the output
-        print(f"The student with the highest GWA is {max_name} with a GWA of {max_gwa:.2f}. Have a good day!")
+        print(f"\n\33[36m The student with the highest GWA is {max_name} with a GWA of {max_gwa:.2f}. Have a good day!")
+        print("\33[41mTerminating Program...\33[41m ")
+        exit()
