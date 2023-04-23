e31920c984eefa1bd5c36c419ede9c7ee4a822fe
Gene014 <calmaeugene06@gmail.com>
2023-04-23 21:53:10 +0800

    resuming on creating variables

--- programcode.py
+++ programcode.py
@@ -6,13 +6,16 @@
 #The program will outputs the name of the student who got the highest GWA (including the GWA).
 
 def process():
+    #set variables for max gwa and for the uname
+    max_gwa=1
+    max_name=""
     #open gwa.txt (read)
     with open("gwa.txt","r") as input_file:
         #read gwa.txt line by line
         for line in input_file:
-            input_gwa = str(line)
-            gwa=(input_gwa)
-            print(gwa.split())
-            edited=(gwa.split())
-            print(edited.strip())
+            variables=line.strip().rsplit('',1)
+            if len(variables) ==2:
+                name, gwa = variables
+                
+            
 process ()
